e4a6c15a97c91bec3597ea25932da68cfb779b12
H. Juan David Romero P <hermes.romero@unipamplona.edu.co>
2020-11-28 20:18:41 -0500

    Update files

--- train.py
+++ train.py
@@ -17,15 +17,6 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 ds = TabularDatasetFactory.from_delimited_files(
     "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
 
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-x_train, x_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.33, random_state=42)
-
-run = Run.get_context()
-
 
 def clean_data(data):
     # Dict for cleaning data
@@ -55,6 +46,18 @@ def clean_data(data):
         lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+
+    return x_df, y_df
+
+
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.33, random_state=42)
+
+run = Run.get_context()
 
 
 def main():
